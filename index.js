99f85b7331071ade3297c05e637532f88037255c
codeinlife10 <codeinlife10@gmail.com>
2020-08-27 18:14:17 +0700

    entrancetest

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const findOppositeNumber = (n,firstNumber) =>{
    if(n <4 || n > 20  || n%2 !=0 || firstNumber < 0 || firstNumber >= n){
        console.log("Error")
    }else{
-       
+        if(firstNumber < n/2){
+           return firstNumber+n/2
+        }else{
+           return firstNumber-n/2
+    }
    }
 }
+console.log(findOppositeNumber(10,2))
+console.log(findOppositeNumber(10,6))
 
 const merge2String = (s1,s2) =>{
     s1 =  s1.split("")
@@ -19,7 +25,7 @@ const merge2String = (s1,s2) =>{
          }
       
      }
-    return s3
+    return s3.filter(number=> number != undefined)
 
 }
 console.log(merge2String("abc","xyz"))
